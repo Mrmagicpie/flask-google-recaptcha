2d115b39ac52c38dbdfef8bb8c2b151b02a984d4
Mrmagicpie <70033286+Mrmagicpie@users.noreply.github.com>
2021-07-06 03:06:15 -0400

    Update __init__.py

--- flask_google_recaptcha/__init__.py
+++ flask_google_recaptcha/__init__.py
@@ -5,47 +5,47 @@ Can be used as standalone
 
 __version__ = "0.5.8"
 __license__ = "MIT"
-__author__ = "Anderson Araujo (coderpy)"
+__author__  = "Anderson Araujo (coderpy)"
 __copyright__ = "(c) 2018 Anderson Araujo (coderpy)"
 
 __all__ = ("GoogleReCaptcha",)
 
-from flask import request
+from json   import loads
+from flask  import request
 from jinja2 import Markup
-from json import loads
 
 try:
     from urllib.request import urlopen
-    from urllib.parse import urlencode
+    from urllib.parse   import urlencode
 
 except ImportError:
     from urllib2 import urlopen
-    from urllib import urlencode
+    from urllib  import urlencode
 
 
 class DEFAULTS(object):
-    IS_ENABLED = True
     THEME = "light"
-    TYPE = "image"
-    SIZE = "normal"
+    TYPE  = "image"
+    SIZE  = "normal"
     TABINDEX = 0
+    IS_ENABLED = True
 
 
 class GoogleReCaptcha(object):
-    VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify"
-    site_key = None
+    site_key   = None
     secret_key = None
     is_enabled = False
+    VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify"
 
     def __init__(self, app=None, site_key=None, secret_key=None, is_enabled=True, **kwargs):
         if site_key:
+            self.type  = kwargs.get('type', DEFAULTS.TYPE)
+            self.size  = kwargs.get('size', DEFAULTS.SIZE)
+            self.theme = kwargs.get('theme', DEFAULTS.THEME)
             self.site_key = site_key
+            self.tabindex = kwargs.get('tabindex', DEFAULTS.TABINDEX)
             self.secret_key = secret_key
             self.is_enabled = is_enabled
-            self.theme = kwargs.get('theme', DEFAULTS.THEME)
-            self.type = kwargs.get('type', DEFAULTS.TYPE)
-            self.size = kwargs.get('size', DEFAULTS.SIZE)
-            self.tabindex = kwargs.get('tabindex', DEFAULTS.TABINDEX)
 
         elif app:
             self.init_app(app=app)
@@ -53,8 +53,7 @@ class GoogleReCaptcha(object):
     def init_app(self, app=None):
         self.__init__(site_key=app.config.get("RECAPTCHA_SITE_KEY"),
                       secret_key=app.config.get("RECAPTCHA_SECRET_KEY"),
-                      is_enabled=app.config.get(
-                          "RECAPTCHA_ENABLED", DEFAULTS.IS_ENABLED),
+                      is_enabled=app.config.get("RECAPTCHA_ENABLED", DEFAULTS.IS_ENABLED),
                       theme=app.config.get("RECAPTCHA_THEME", DEFAULTS.THEME),
                       type=app.config.get("RECAPTCHA_TYPE", DEFAULTS.TYPE),
                       size=app.config.get("RECAPTCHA_SIZE", DEFAULTS.SIZE),
@@ -66,8 +65,8 @@ class GoogleReCaptcha(object):
 
     def get_code(self):
         """
-        Returns the new ReCaptcha code
-        :return:
+        
+        :return: Returns the new ReCaptcha code
         """
         return "" if not self.is_enabled else ("""
         <script src='//www.google.com/recaptcha/api.js'></script>
@@ -79,20 +78,15 @@ class GoogleReCaptcha(object):
         if self.is_enabled:
             data = {
                 "secret": self.secret_key,
-                "response": response or request.form.get('g-recaptcha-response'),
+                "response": response  or request.form.get('g-recaptcha-response'),
                 "remoteip": remote_ip or request.environ.get('REMOTE_ADDR')
             }
 
             resp = urlopen(self.VERIFY_URL + "?" + urlencode(data))
-
             data = resp.read()
-
             content = loads(data)
 
-            if content["success"] or content["error-codes"][0] == "timeout-or-duplicate" and resp.code == 200:
-                return True
-
-            else:
+            if not content["success"] or content["error-codes"][0] != "timeout-or-duplicate" and resp.code != 200:
                 return False
 
         return True
